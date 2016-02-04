f581875dca4d8c4d2809c774cb7f1c66308716e7
杨佳辉 <yangjh@lotuseed.com>
2016-02-04 16:24:42 +0800

    init

---
+++ Example/Example/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Example
+//
+//  Created by 杨 on 16/2/4.
+//  Copyright © 2016年 杨. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
